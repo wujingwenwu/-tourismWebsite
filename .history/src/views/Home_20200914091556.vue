23803fe7efd0c3d717af2e985ba92ae87b3593ba
“wujingwenwu” <1414727570@qq.com>
2020-09-14 09:39:32 +0800

    旅游网

---
+++ .history/src/views/Home_20200914091556.vue
@@ -0,0 +1,34 @@
+<template>
+ <div>
+
+ </div>
+</template>
+
+<script lang='ts'>
+import {defineComponent, reactive, toRefs, SetupContext} from 'vue'
+interface Data {
+  age:string
+}
+ export default defineComponent({
+   name: '',
+   props: {
+   },
+   components: {
+
+   },
+setup(props, ctx: SetupContext){
+
+let data: Data = reactive<Data>({
+  age:'你好'
+
+})
+return {
+...toRefs(data),
+}
+},
+ })
+</script>
+
+<style scoped lang='scss'>
+
+</style>
---
+++ .history/src/views/Home_20200914091737.vue
@@ -0,0 +1,33 @@
+<template>
+ <div>
+
+ </div>
+</template>
+
+<script lang='ts'>
+import {defineComponent, reactive, toRefs, SetupContext} from 'vue'
+interface Data {
+  age:string
+}
+ export default defineComponent({
+   name: '',
+   props: {
+   },
+   components: {
+
+   },
+setup(){
+let data: Data = reactive<Data>({
+  age:'你好',
+
+})
+return {
+...toRefs(data),
+}
+},
+ })
+</script>
+
+<style scoped lang='scss'>
+
+</style>
